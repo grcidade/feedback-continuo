430e2a94df2e556ca56b408c3b005921d4558fd9
Guilherme Cidade <guirevcidade1@gmail.com>
2022-04-28 15:34:23 -0300

    fix: updating profile image

--- src/main/java/br/com/dbc/vimserdev/feedbackcontinuo/repositories/UserRepository.java
+++ src/main/java/br/com/dbc/vimserdev/feedbackcontinuo/repositories/UserRepository.java
@@ -11,5 +11,5 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<UserEntity, String> {
 
     Optional<UserEntity> findByEmail(String email);
-    List<UserEntity> findAllByUserIdIsNotAndUserIdIsNot(String userId, String userid);
+    List<UserEntity> findAllByUserIdIsNot(String userId);
 }
