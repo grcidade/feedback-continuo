81c1df70e81dec20c38dbc55e5e5ea4ad5b0239f
Guilherme Cidade <guirevcidade1@gmail.com>
2022-05-01 00:33:29 -0300

    feat: new testes

--- src/test/java/br/com/dbc/vimserdev/feedbackcontinuo/services/UserServiceTest.java
+++ src/test/java/br/com/dbc/vimserdev/feedbackcontinuo/services/UserServiceTest.java
@@ -1,9 +1,11 @@
 package br.com.dbc.vimserdev.feedbackcontinuo.services;
 
+import br.com.dbc.vimserdev.feedbackcontinuo.dtos.ForgotPasswordHandlerDTO;
 import br.com.dbc.vimserdev.feedbackcontinuo.dtos.UserCreateDTO;
 import br.com.dbc.vimserdev.feedbackcontinuo.entities.UserEntity;
 import br.com.dbc.vimserdev.feedbackcontinuo.exception.BusinessRuleException;
 import br.com.dbc.vimserdev.feedbackcontinuo.repositories.UserRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,10 +16,12 @@ import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.util.Optional;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
@@ -26,6 +30,9 @@ public class UserServiceTest {
 
     @Mock
     private UserRepository userRepository;
+
+    @Mock
+    private ForgotPasswordHandlerProducerService passwordHandlerProducerService;
 
     private final Authentication authentication = Mockito.mock(Authentication.class);
     private final SecurityContext securityContext = Mockito.mock(SecurityContext.class);
@@ -90,7 +97,25 @@ public class UserServiceTest {
 
         UserEntity user = UserEntity.builder()
                 .email("a@dbccompany.com.br")
-                .password("Senha@123").build();
+                .password(new BCryptPasswordEncoder().encode("Senha@123")).build();
+
+        SecurityContextHolder.setContext(securityContext);
+
+        when(userRepository.findById(anyString())).thenReturn(Optional.of(user));
+
+        try{
+            userService.changePasswordUserLoged("Senha@1", "123");
+        }catch (BusinessRuleException e){
+            assertEquals("Senha antiga incompatível.", e.getMessage());
+        }
+    }
+
+    @Test
+    public void deveDarErroSenhaFraca(){
+
+        UserEntity user = UserEntity.builder()
+                .email("a@dbccompany.com.br")
+                .password(new BCryptPasswordEncoder().encode("Senha@123")).build();
 
         SecurityContextHolder.setContext(securityContext);
 
@@ -99,11 +124,125 @@ public class UserServiceTest {
         try{
             userService.changePasswordUserLoged("Senha@123", "123");
         }catch (BusinessRuleException e){
-            assertEquals("Senha antiga incompatível.", e.getMessage());
+            assertEquals("Senha fraca demais", e.getMessage());
         }
+    }
 
+    @Test
+    public void deveDarErroSenhaAtualIgual(){
+
+        UserEntity user = UserEntity.builder()
+                .email("a@dbccompany.com.br")
+                .password(new BCryptPasswordEncoder().encode("Senha@123")).build();
+
+        SecurityContextHolder.setContext(securityContext);
+
+        when(userRepository.findById(anyString())).thenReturn(Optional.of(user));
+
+        try{
+            userService.changePasswordUserLoged("Senha@123", "Senha@123");
+        }catch (BusinessRuleException e){
+            assertEquals("Essa já é sua senha atual!", e.getMessage());
+        }
+    }
+
+    @Test
+    public void deveChamarSalvarNoTrocarSenha() throws BusinessRuleException {
+
+        UserEntity user = UserEntity.builder()
+                .email("a@dbccompany.com.br")
+                .password(new BCryptPasswordEncoder().encode("Senha@123")).build();
+
+        SecurityContextHolder.setContext(securityContext);
+
+        when(userRepository.findById(anyString())).thenReturn(Optional.of(user));
+
+        userService.changePasswordUserLoged("Senha@123", "Senha@12345");
+
+        verify(userRepository, times(1)).save(user);
 
     }
 
+    @Test
+    public void deveDarErroEmailMetodoEsqueciSenha() throws JsonProcessingException {
+        try{
+            userService.forgotPassword("joao@dbccompany.com");
+        }catch (BusinessRuleException e){
+            assertEquals("Email inválido.",e.getMessage());
+        }
+    }
+
+    @Test
+    public void deveDarErroUsuarioNaoEncontradoMetodoEsqueciSenha() throws JsonProcessingException {
+
+        when(userRepository.findByEmail(anyString())).thenReturn(Optional.empty());
+
+        try{
+            userService.forgotPassword("joao@dbccompany.com.br");
+        }catch (BusinessRuleException e){
+            assertEquals("Usuário não encontrado.",e.getMessage());
+        }
+    }
+
+    @Test
+    public void deveChamarSalvarMetodoEsqueciSenha() throws BusinessRuleException, JsonProcessingException {
+
+        UserEntity user = UserEntity.builder()
+                .email("joao@dbccompany.com.br")
+                .password(new BCryptPasswordEncoder().encode("Senha@123")).build();
+
+        when(userRepository.findByEmail(anyString())).thenReturn(Optional.of(user));
+        doNothing().when(passwordHandlerProducerService).send(any(ForgotPasswordHandlerDTO.class));
+
+        userService.forgotPassword("joao@dbccompany.com.br");
+
+        verify(userRepository, times(1)).save(any(UserEntity.class));
+    }
+
+    @Test
+    public void deveChamarProducerServiceMetodoEsqueciSenha() throws BusinessRuleException, JsonProcessingException {
+
+        UserEntity user = UserEntity.builder()
+                .email("joao@dbccompany.com.br")
+                .password(new BCryptPasswordEncoder().encode("Senha@123")).build();
+
+        when(userRepository.findByEmail(anyString())).thenReturn(Optional.of(user));
+
+        userService.forgotPassword("joao@dbccompany.com.br");
+
+        verify(passwordHandlerProducerService, times(1)).send(any(ForgotPasswordHandlerDTO.class));
+    }
+
+    @Test
+    public void deveTestarChamadaFindByEmail(){
+        userService.findByEmail(anyString());
+        verify(userRepository,times(1)).findByEmail(anyString());
+    }
+
+    @Test
+    public void deveTestarChamadaAuthentication(){
+
+        SecurityContextHolder.setContext(securityContext);
+
+        userService.getLogedUserId();
+        verify(authentication,times(1)).getPrincipal();
+    }
+
+    @Test
+    public void deveChamarFindAllByUserSemUserLogado(){
+
+        SecurityContextHolder.setContext(securityContext);
+
+        userRepository.findAllByUserIdIsNot(anyString());
+
+        verify(userRepository, times(1)).findAllByUserIdIsNot(anyString());
+
+    }
+
+    @Test
+    public void deveDevolverNullUserLogado(){
+        SecurityContextHolder.setContext(securityContext);
+        assertNull(userService.getLogedUser());
+    }
 
 }
